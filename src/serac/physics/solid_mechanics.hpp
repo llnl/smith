94eafbfe8571e8882eed5b5737f4ccd3f6dc8d2b
Michael Tupek <135926736+tupek2@users.noreply.github.com>
2024-09-12 10:18:32 -0700

    Update solid_mechanics.hpp
    Delete copy

--- src/serac/physics/solid_mechanics.hpp
+++ src/serac/physics/solid_mechanics.hpp
@@ -1190,9 +1190,6 @@ public:
     // Build the dof array lookup tables
     displacement_.space().BuildDofToArrays();
 
-    u_ = displacement_;
-    v_ = velocity_;
-
     if (is_quasistatic_) {
       residual_with_bcs_ = buildQuasistaticOperator();
     } else {
