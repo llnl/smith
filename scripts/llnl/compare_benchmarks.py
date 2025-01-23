d1acac76bf4805f1293c0a3448edecce2e71c39e
Alex Tyler Chapman <chapman39@llnl.gov>
2025-01-22 16:16:24 -0800

    setting up graph frames

--- scripts/llnl/compare_benchmarks.py
+++ scripts/llnl/compare_benchmarks.py
@@ -8,53 +8,73 @@
 #
 # Original Source: https://llnl-hatchet.readthedocs.io/en/latest/llnl.html#id3
 
+from common_build_functions import *
+
 import sys
 import os
 import platform
 import datetime as dt
 from IPython.display import HTML, display
+from argparse import ArgumentParser
 
 input_deploy_dir_str = "/usr/gapps/spot/live/"
 machine = platform.uname().machine
-
 sys.path.append(input_deploy_dir_str + "/hatchet-venv/" + machine + "/lib/python3.7/site-packages")
 sys.path.append(input_deploy_dir_str + "/hatchet/" + machine)
 sys.path.append(input_deploy_dir_str + "/spotdb")
-
 import hatchet
 import spotdb
 
-# Find benchmarks run on this branch `your_benchmarks`
-# OR
-# Use given caliper file (to compare benchmarks one at a time)
+def parse_args():
+    "Parses args from command line"
+    parser = ArgumentParser()
+    parser.add_argument("-bd", "--build-directory",
+                      dest="build_dir",
+                      help="Path to a Serac build containing caliper files (make sure it's Release and benchmarks are enabled!)")
+    parser.add_argument("-sd", "--spot-directory",
+                      dest="spot_dir",
+                      default=get_shared_spot_dir(),
+                      help="Where to put all resulting caliper files to use for SPOT analysis (defaults to a shared location)")
 
-# Find latest benchmarks in shared spot location `latest_shared_benchmarks`
+    # Parse args
+    args, _ = parser.parse_known_args()
+    args = vars(args)
 
-# Iterate latest benchmarks list
-    # Find associated branch benchmark
-    # diff = PR - develop, if the `main` number is bigger, it means it took X more seconds
+    return args
 
-input_db_uri_str = "/usr/workspace/meemee/serac/repo/build-ruby-toss_4_x86_64_ib-gcc@10.3.1-release"
-input_run_ids_str = "{0},{1}".format(
-    os.path.join(input_db_uri_str, "physics-benchmarks-functional-baseline.cali"),
-    os.path.join(input_db_uri_str, "physics-benchmarks-functional-PR.cali"))
+def get_benchmark_name(gf):
+    """Get benchmark name from a graph frame"""
+    executable = str(gf.metadata.get("executable", 1))
+    job_size = int(gf.metadata.get("jobsize", 1)) 
+    return "{0}_{1}".format(executable, job_size)
 
-db = spotdb.connect(input_db_uri_str)
-runs = input_run_ids_str.split(',')
+def main():
+    # setup
+    args = parse_args()
 
-gfs = hatchet.GraphFrame.from_spotdb(db, runs)
+    build_dir = args["build_dir"]
+    spot_dir = args["spot_dir"]
 
-gf_diff = gfs[1] - gfs[0]
-#gf_diff.drop_index_levels()
+    # TODO
+    gfs_current = [] # build_dir, locally generated caliper files 
 
-# print(gf_diff.dataframe)
-# print(gf_diff.tree(rank=0))
+    # Setup baseline (shared SPOT) graph frames
+    # Only take caliper files from the previous week
+    baseline_calis = os.listdir(spot_dir)
+    baseline_calis.sort(reverse=True)
+    baseline_cali_date = baseline_calis[0].split('-')[0]
+    delete_index = 0
+    for i in range(len(baseline_calis)):
+        if baseline_cali_date in baseline_calis[i]:
+            baseline_calis[i] = os.path.join(spot_dir, baseline_calis[0])
+        else:
+            delete_index = i
+            break
+    del baseline_calis[delete_index:]
+    db = spotdb.connect(spot_dir)
+    gfs_baseline = hatchet.GraphFrame.from_spotdb(db, baseline_calis)
 
-print(gf_diff.tree(depth=1))
+    # TODO Setup current (local build dir) graph frames
 
-# for idx, gf in enumerate(gfs):
-#     launchdate = dt.datetime.fromtimestamp(int(gf.metadata["launchdate"]))
-#     jobsize = int(gf.metadata.get("jobsize", 1))
-#     print("launchdate: {}, jobsize: {}".format(launchdate, jobsize))
-#     print(gf.tree())
-#     display(HTML(gf.dataframe.to_html()))
+if __name__ == "__main__":
+    sys.exit(main())
