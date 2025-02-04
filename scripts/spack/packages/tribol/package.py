2db1352df59d5d200ba63512ade42f8b8d7e4b26
Chris White <white238@llnl.gov>
2025-02-03 16:31:44 -0800

    update tribol

--- scripts/spack/packages/tribol/package.py
+++ scripts/spack/packages/tribol/package.py
@@ -44,7 +44,7 @@ class Tribol(CachedCMakePackage, CudaPackage, ROCmPackage):
     # SERAC EDIT START
     # Note: We add a number to the end of the real version number to indicate that we have
     #  moved forward past the release. Increment the last number when updating the commit sha.
-    version("0.1.0.17", commit="165ae273fcfef27bb96321c0a454382d1993ad6f", submodules=True, preferred=True)
+    version("0.1.0.17", commit="ed9247387bc54aab78362dd81339b6709bcaff51", submodules=True, preferred=True)
     # SERAC EDIT END
 
     # -----------------------------------------------------------------------
