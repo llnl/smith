e930fa76b42a6e78a9935d6f1ae816e677b9214c
Brandon Talamini <talamini1@llnl.gov>
2025-02-08 11:59:06 -0800

    Add rate sensitivity parameter to input parsing

--- src/serac/physics/materials/hardening_input.cpp
+++ src/serac/physics/materials/hardening_input.cpp
@@ -13,6 +13,7 @@ void HardeningInputOptions::defineInputFileSchema(axom::inlet::Container& contai
   // Shared between both hardening laws
   container.addString("law", "Name of the hardening law (e.g. PowerLawHardening)").required(true);
   container.addDouble("sigma_y", "Yield strength");
+  container.addDouble("eta", "Plastic viscosity");
 
   // PowerLawHardening
   container.addDouble("n", "Hardening index in reciprocal form");
@@ -30,12 +31,13 @@ void HardeningInputOptions::defineInputFileSchema(axom::inlet::Container& contai
     bool eps0_present = c.contains("eps0") && (c["eps0"].type() == double_type);
     bool sigma_sat_present = c.contains("sigma_sat") && (c["sigma_sat"].type() == double_type);
     bool strain_constant_present = c.contains("strain_constant") && (c["strain_constant"].type() == double_type);
+    bool eta_present = c.contains("eta") && (c["eta"].type() == double_type);
 
     std::string law = c["law"];
     if (law == "PowerLawHardening") {
-      return sigma_y_present && n_present && eps0_present && !sigma_sat_present && !sigma_sat_present;
+      return sigma_y_present && n_present && eps0_present && eta_present && !sigma_sat_present && !sigma_sat_present;
     } else if (law == "VoceHardening") {
-      return sigma_y_present && !n_present && !eps0_present && sigma_sat_present && strain_constant_present;
+      return sigma_y_present && eta_present && !n_present && !eps0_present && sigma_sat_present && strain_constant_present;
     }
 
     return false;
@@ -50,10 +52,10 @@ serac::var_hardening_t FromInlet<serac::var_hardening_t>::operator()(const axom:
   std::string law = base["law"];
   if (law == "PowerLawHardening") {
     result =
-        serac::solid_mechanics::PowerLawHardening{.sigma_y = base["sigma_y"], .n = base["n"], .eps0 = base["eps0"]};
+        serac::solid_mechanics::PowerLawHardening{.sigma_y = base["sigma_y"], .n = base["n"], .eps0 = base["eps0"], .eta = base["eta"]};
   } else if (law == "VoceHardening") {
     result = serac::solid_mechanics::VoceHardening{
-        .sigma_y = base["sigma_y"], .sigma_sat = base["sigma_sat"], .strain_constant = base["strain_constant"]};
+        .sigma_y = base["sigma_y"], .sigma_sat = base["sigma_sat"], .strain_constant = base["strain_constant"], .eta = base["eta"]};
   }
   return result;
 }
