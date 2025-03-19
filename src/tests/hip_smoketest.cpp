a4d56ff266e6ca0140ca578736bc9e7e514ff933
Chris White <white238@llnl.gov>
2025-03-19 13:38:20 -0700

    Update src/tests/hip_smoketest.cpp
    Co-authored-by: Alex Tyler Chapman <100869159+chapman39@users.noreply.github.com>

--- src/tests/hip_smoketest.cpp
+++ src/tests/hip_smoketest.cpp
@@ -63,6 +63,7 @@ vectoradd_float(float* __restrict__ a, const float* __restrict__ b, const float*
       // SERAC EDIT START
       int x = static_cast<int>(hipBlockDim_x * hipBlockIdx_x + hipThreadIdx_x);
       int y = static_cast<int>(hipBlockDim_y * hipBlockIdx_y + hipThreadIdx_y);
+      // SERAC EDIT END
 
       int i = y * width + x;
       if ( i < (width * height)) {
