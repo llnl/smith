44e590648423d5599ee5e4bdf7743516c93cc9e3
Alex Chapman <chapman39@llnl.gov>
2025-05-07 14:40:02 -0700

    use older cmake, attempt to fix emails sending false-positives

--- scripts/shared-macmini/build-and-test.sh
+++ scripts/shared-macmini/build-and-test.sh
@@ -2,7 +2,8 @@
 
 # Build and test Serac on team's shared MacMini, then report results to a set of emails
 
-# Update environment
+# Immediately fail upon error and update environment
+set -e
 source ~/.bash_profile
 
 # Variables
@@ -10,7 +11,17 @@ CI_ROOT_DIR="/Users/chapman39/dev/serac/ci"
 PROJECT_DIR="$CI_ROOT_DIR/repo"
 OUTPUT_LOG="$CI_ROOT_DIR/logs/macmini-build-and-test-$(date +"%Y_%m_%d_%H_%M_%S").log"
 HOST_CONFIG="$CI_ROOT_DIR/host-configs/firion-darwin-sonoma-aarch64-clang@14.0.6.cmake"
-RECIPIENTS="chapman39@llnl.gov,white238@llnl.gov,talamini1@llnl.gov"
+# RECIPIENTS="chapman39@llnl.gov,white238@llnl.gov,talamini1@llnl.gov"
+RECIPIENTS="chapman39@llnl.gov"
+EMAIL_SUBJECT="Serac Failed! MacMini build and test report $(date)"
+EMAIL_BODY="This is automatic weekly report of Serac's MacMini build. See attached for log."
+
+function send_email() {
+    echo "$EMAIL_BODY" | print_run_log mutt -a "$OUTPUT_LOG" -s "$EMAIL_SUBJECT" -- "$RECIPIENTS"
+}
+
+# Send email before exiting from an error
+trap "send_email; exit 1" ERR
 
 print_run_log(){
     echo "####################" >> "$OUTPUT_LOG"
@@ -34,14 +45,6 @@ print_run_log rm -rf _serac_build_and_test*
 # Build and test Serac
 print_run_log python3 ./scripts/llnl/build_src.py --host-config $HOST_CONFIG -v -j16
 
-# Email variables
-if [ $? -eq 0 ]; then
-    EMAIL_SUBJECT="Serac Succeeded!"
-else
-    EMAIL_SUBJECT="Serac Failed!"
-fi
-EMAIL_SUBJECT="$EMAIL_SUBJECT MacMini build and test report $(date)"
-EMAIL_BODY="This is automatic weekly report of Serac's MacMini build. See attached for log."
-
-# Send report via email
-echo "$EMAIL_BODY" | print_run_log mutt -a "$OUTPUT_LOG" -s "$EMAIL_SUBJECT" -- "$RECIPIENTS"
+# Update email subject to indicate success and send
+EMAIL_SUBJECT="Serac Succeeded! MacMini build and test report $(date)"
+send_email
