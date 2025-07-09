e36b24e101f93329339a081fe5da3eb647d2b450
thartland <tucker.hartland@gmail.com>
2025-07-09 15:07:35 -0700

    updates to constraint interface based on conversion with Mike and Eric in anticipation of e.g., contact problems with a non-symmetric formulation

--- src/serac/physics/constraints.hpp
+++ src/serac/physics/constraints.hpp
@@ -7,7 +7,8 @@
 /**
  * @file constraints.hpp
  *
- * @brief Specifies interface for evaluating distributed constriants from fields and their field gradients
+ * @brief Specifies interface for evaluating distributed constriants from fields as well as
+ * their Jacobians and Hessian-vector products
  */
 
 #pragma once
@@ -34,38 +35,95 @@ class Constraint {
   /// @brief destructor
   virtual ~Constraint() {}
 
-  /** @brief Virtual interface for computing the scale value for the objective/constrant, given a vector of
+  /** @brief Virtual interface for computing the constraint, given a vector of
    * serac::FiniteElementState*
    *
    * @param time time
    * @param dt time step
-   * @param fields vector of serac::FiniteElementState* as arguments to the residual
+   * @param vector of serac::FiniteElementState*
    * @return mfem::Vector which is the constraint evaluation
    */
   virtual mfem::Vector evaluate(double time, double dt, const std::vector<ConstFieldPtr>& fields) const = 0;
 
-  /** @brief Virtual interface for computing objective gradient from a vector of serac::FiniteElementState*
+  /** @brief Virtual interface for computing constraint Jacobian from a vector of serac::FiniteElementState*
    *
    * @param time time
    * @param dt time step
-   * @param fields vector of serac::FiniteElementState* as arguments to the residual
+   * @param vector of serac::FiniteElementState*
    * @param direction index for which field to take the gradient with respect to
    * @return std::unique_ptr<mfem::HypreParMatrix>
    */
   virtual std::unique_ptr<mfem::HypreParMatrix> jacobian(double time, double dt,
                                                          const std::vector<ConstFieldPtr>& fields,
                                                          int direction) const = 0;
-
-  /** @brief constraint Hessian-vector productVirtual interface for computing objective gradient from a vector of
+  
+  /** @brief Virtual interface for computing constraint Jacobian_tilde from a vector of serac::FiniteElementState*
+   *
+   * @param time time
+   * @param dt time step
+   * @param vector of serac::FiniteElementState*
+   * @param direction index for which field to take the gradient with respect to
+   * @return std::unique_ptr<mfem::HypreParMatrix>
+   */
+  std::unique_ptr<mfem::HypreParMatrix> jacobian_tilde(double time, double dt,
+                                                         const std::vector<ConstFieldPtr>& fields,
+                                                         int direction)
+  {
+    return jacobian(time, dt, fields, direction);
+  };
+  
+  /** @brief Virtual interface for computing residual contribution Jacobian_tilde^T multiplier from a vector of serac::FiniteElementState*
+   *
+   * @param time time
+   * @param dt time step
+   * @param vector of serac::FiniteElementState*
+   * @param mfem::Vector of Lagrange multipliers
+   * @param direction index for which field to take the gradient with respect to
+   * @return std::Vector
+   */
+  mfem::Vector residual_contribution(double time, double dt,
+                                                         const std::vector<ConstFieldPtr>& fields,
+							 const mfem::Vector & multipliers,
+                                                         int direction)
+  {
+    std::unique_ptr<mfem::HypreParMatrix> jac = jacobian_tilde(time, dt, fields, direction);
+    mfem::Vector y(jac->Width()); y = 0.0;
+    SLIC_ERROR_ROOT_IF(jac->Height() != multipliers.Size(), 
+		       "Incompatible matrix and vector sizes.");
+    jac->MultTranspose(multipliers, y);
+    return y;
+  };
+  
+  /** @brief Virtual interface for computing Jacobians of the residual contribution from a vector of
    * serac::FiniteElementState*
    *
    * @param time time
    * @param dt time step
-   * @param fields vector of serac::FiniteElementState* as arguments to the residual
+   * @param vector of serac::FiniteElementState*
+   * @param mfem::Vector of multipliers
+   * @param direction index for which field to take the gradient with respect to
    * @return std::unique_ptr<mfem::HypreParMatrix>
    */
-  virtual std::unique_ptr<mfem::HypreParMatrix> hvp(double time, double dt, const std::vector<ConstFieldPtr>& fields);
+  virtual std::unique_ptr<mfem::HypreParMatrix> residual_contribution_jacobian(double time, double dt, 
+                                                    const std::vector<ConstFieldPtr>& fields,
+						    const mfem::Vector &multipliers,
+                                                    int direction);
 
+  /** @brief Virtual interface for computing constraint Hessian-vector product from a vector of
+   * serac::FiniteElementState*
+   *
+   * @param time time
+   * @param dt time step
+   * @param vector of serac::FiniteElementState*
+   * @param mfem::Vector of multipliers
+   * @param direction index for which field to take the gradient with respect to
+   * @return std::unique_ptr<mfem::HypreParMatrix>
+   */
+  virtual std::unique_ptr<mfem::HypreParMatrix> hvp(double time, double dt, 
+                                                    const std::vector<ConstFieldPtr>& fields,
+						    const mfem::Vector& multipliers,
+                                                    int direction);
+  
   /// @brief name
   std::string name() const { return name_; }
 
