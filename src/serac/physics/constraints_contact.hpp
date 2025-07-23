70e9310e662fb66fd2b6f97d0dde6d2f19ed95c1
thartland <tucker.hartland@gmail.com>
2025-07-23 13:01:12 -0700

    adding initial cut on contact constraints

---
+++ src/serac/physics/constraints_contact.hpp
@@ -0,0 +1,93 @@
+// Copyright Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+/**
+ * @file constraints.hpp
+ *
+ * @brief Specifies interface for evaluating distributed constriants from fields as well as
+ * their Jacobians and Hessian-vector products
+ */
+
+#pragma once
+
+#include <vector>
+#include "serac/physics/common.hpp"
+#include "serac/physics/field_types.hpp"
+
+namespace mfem {
+class Vector;
+class HypreParMatrix;
+}  // namespace mfem
+
+namespace serac {
+
+class FiniteElementState;
+
+/// @brief Abstract constraint class
+class ContactConstraint : public Constraint {
+ public:
+  /// @brief base constructor takes the name of the physics
+  ContactConstraint(const mfem::ParMesh & mesh, const std::string& name) : Constraint(name), mesh_{mesh} 
+  {
+    contact_(mesh_);
+  }
+
+  /// @brief destructor
+  virtual ~ContactConstraint() {}
+
+  ///** @brief Interface for computing the gap contact constraint, given a vector of
+  // * serac::FiniteElementState*
+  // *
+  // * @param time time
+  // * @param dt time step
+  // * @param fields vector of serac::FiniteElementState*
+  // * @return mfem::Vector which is the constraint evaluation
+  // */
+  //mfem::Vector evaluate(double time, double dt, const std::vector<ConstFieldPtr>& fields) 
+  //{
+  //   //TODO: complete me 
+  //};
+
+  ///** @brief Interface for computing contact gap constraint Jacobian from a vector of serac::FiniteElementState*
+  // *
+  // * @param time time
+  // * @param dt time step
+  // * @param fields vector of serac::FiniteElementState*
+  // * @param direction index for which field to take the gradient with respect to
+  // * @return std::unique_ptr<mfem::HypreParMatrix>
+  // */
+  //std::unique_ptr<mfem::HypreParMatrix> jacobian(double time, double dt,
+  //                                                       const std::vector<ConstFieldPtr>& fields,
+  //                                                       int direction) const
+  //{
+  //   // TODO: complete me
+  //};
+  
+  /**
+   * @brief Add a mortar contact boundary condition
+   *
+   * @param interaction_id Unique identifier for the ContactInteraction
+   * @param bdry_attr_surf1 MFEM boundary attributes for the first surface
+   * @param bdry_attr_surf2 MFEM boundary attributes for the second surface
+   * @param contact_opts Defines contact method, enforcement, type, and penalty
+   */
+  void addContactInteraction(int interaction_id, const std::set<int>& bdry_attr_surf1,
+                             const std::set<int>& bdry_attr_surf2, ContactOptions contact_opts)
+  {
+    //SLIC_ERROR_ROOT_IF(!is_quasistatic_, "Contact can only be applied to quasistatic problems.");
+    //SLIC_ERROR_ROOT_IF(order > 1, "Contact can only be applied to linear (order = 1) meshes.");
+    contact_.addContactInteraction(interaction_id, bdry_attr_surf1, bdry_attr_surf2, contact_opts);
+  }
+
+
+ protected:
+  /// @brief Class holding contact constraint data
+  ContactData contact_;
+ private:
+  const mfem::ParMesh & mesh_;
+};
+
+}  // namespace serac
