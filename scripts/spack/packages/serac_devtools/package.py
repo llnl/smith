40923e009ca38a8ea5c457e76136a56bf6519e38
Chris White <white238@llnl.gov>
2025-08-27 22:07:11 -0700

    revert devtools updates

--- scripts/spack/packages/serac_devtools/package.py
+++ scripts/spack/packages/serac_devtools/package.py
@@ -10,17 +10,11 @@ class SeracDevtools(BundlePackage):
 
     version('fakeversion')
 
-    variant('compiler_only', default=False, description="Build only required compiler with Enzyme")
-
-    depends_on("llvm@19+clang")
-    depends_on("llvm+python", when="~compiler_only")
-
-    depends_on('cmake', when="~compiler_only")
-    depends_on('cppcheck', when="~compiler_only")
-    depends_on('doxygen', when="~compiler_only")
+    depends_on('cmake')
+    depends_on('cppcheck')
+    depends_on('doxygen')
     # Disabled due to integration tests being disabled
-    # depends_on('py-ats', when="~compiler_only")
-    depends_on('py-sphinx', when="~compiler_only")
-    depends_on('python', when="~compiler_only")
-    
-
+    # depends_on('py-ats')
+    depends_on('py-sphinx')
+    depends_on('python')
+    depends_on("llvm@19+clang+python")
