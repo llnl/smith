d76149885701d0ce8734b03df714f327946e3d96
chapman39 <chapman39@llnl.gov>
2025-10-15 15:06:10 -0700

    update cray mpich version

--- scripts/spack/packages/cray-mpich/package.py
+++ scripts/spack/packages/cray-mpich/package.py
@@ -8,7 +8,7 @@ from spack_repo.builtin.packages.cray_mpich.package import CrayMpich as BuiltinC
 
 class CrayMpich(BuiltinCrayMpich):
     # Adds newer version
-    version('8.1.13')
+    version("8.1.29")
 
     # https://github.com/spack/spack/blob/f50f5859f31d7ba76e044039253fdb1689ea017a/lib/spack/spack/build_systems/cached_cmake.py#L230-L234
     variant("slurm", default=True, description="Added to get MPIEXEC_NUMPROC_FLAG right")
