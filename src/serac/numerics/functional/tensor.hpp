8f9759728fd03edd5372aae4fa1091953bf9a212
chvillanuevap <villanueva18@llnl.gov>
2025-10-23 23:35:59 -0700

    Add epsilon to normalize function

--- src/serac/numerics/functional/tensor.hpp
+++ src/serac/numerics/functional/tensor.hpp
@@ -1114,9 +1114,9 @@ SERAC_HOST_DEVICE constexpr auto norm(zero) { return zero{}; }
  * @param[in] A The tensor to normalize
  */
 template <typename T, int... n>
-SERAC_HOST_DEVICE auto normalize(const tensor<T, n...>& A)
+SERAC_HOST_DEVICE auto normalize(const tensor<T, n...>& A, double const &epsilon = 0.0)
 {
-  return A / norm(A);
+  return A / (norm(A) + epsilon);
 }
 
 /**
@@ -1337,7 +1337,7 @@ SERAC_HOST_DEVICE constexpr auto detApIm1(const tensor<T, 3, 3>& A)
 
   // clang-format off
   // equivalent to tr(A) + I2(A) + det(A)
-  return A(0, 0) + A(1, 1) + A(2, 2) 
+  return A(0, 0) + A(1, 1) + A(2, 2)
        - A(0, 1) * A(1, 0) * (1 + A(2, 2))
        + A(0, 0) * A(1, 1) * (1 + A(2, 2))
        - A(0, 2) * A(2, 0) * (1 + A(1, 1))
@@ -2088,7 +2088,7 @@ inline mat < 2, 2 > look_at(const vec < 2 > & direction) {
 
 inline mat < 3, 3 > R3_basis(const vec3 & n) {
   float sign = (n[2] >= 0.0f) ? 1.0f : -1.0f;
-  float a = -1.0f / (sign + n[2]); 
+  float a = -1.0f / (sign + n[2]);
   float b = n[0] * n[1] * a;
 
   return mat < 3, 3 >{
