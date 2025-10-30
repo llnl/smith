3788895d5f58626a81973d2832a2db04ee6a22f6
Alex Chapman <chapman39@llnl.gov>
2025-10-30 15:46:50 -0700

    update to use smith name

--- scripts/shared-macmini/build-and-test.sh
+++ scripts/shared-macmini/build-and-test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Build and test Serac on team's shared MacMini, then report results to a set of emails
+# Build and test Smith on team's shared MacMini, then report results to a set of emails
 
 # Immediately fail upon error and update environment
 set -e
 source ~/.bash_profile
 
 # Variables
-CI_ROOT_DIR="/Users/chapman39/dev/serac/.ci"
+CI_ROOT_DIR="/Users/chapman39/dev/.ci/smith"
 PROJECT_DIR="$CI_ROOT_DIR/repo"
 OUTPUT_LOG="$CI_ROOT_DIR/logs/macmini-build-and-test-$(date +"%Y_%m_%d_%H_%M_%S").log"
 HOST_CONFIG="$PROJECT_DIR/host-configs/other/firion-*.cmake"
 RECIPIENTS="chapman39@llnl.gov,white238@llnl.gov,talamini1@llnl.gov"
-EMAIL_SUBJECT="Serac Failed! MacMini build and test report $(date)"
-EMAIL_BODY="This is automatic weekly report of Serac's MacMini build. See attached for log."
+EMAIL_SUBJECT="Smith Failed! MacMini build and test report $(date)"
+EMAIL_BODY="This is automatic weekly report of Smith's MacMini build. See attached for log."
 
 function send_email() {
     echo "$EMAIL_BODY" | print_run_log mutt -a "$OUTPUT_LOG" -s "$EMAIL_SUBJECT" -- "$RECIPIENTS"
@@ -34,17 +34,18 @@ print_run_log(){
 # Go to project directory
 print_run_log cd $PROJECT_DIR
 
-# Update Serac
+# Update repo
 print_run_log git checkout develop
 print_run_log git pull
 print_run_log git submodule update --init --recursive
 
 # Clear previous build(s)
 print_run_log rm -rf _serac_build_and_test*
+print_run_log rm -rf _smith_build_and_test*
 
-# Build and test Serac
+# Build and test
 print_run_log python3 ./scripts/llnl/build_src.py --host-config $HOST_CONFIG -v -j16
 
 # Update email subject to indicate success and send
-EMAIL_SUBJECT="Serac Succeeded! MacMini build and test report $(date)"
+EMAIL_SUBJECT="Smith Succeeded! MacMini build and test report $(date)"
 send_email
