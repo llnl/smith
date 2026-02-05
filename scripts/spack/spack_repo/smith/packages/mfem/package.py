9dca29fca7bba1f247f81239e17a9e0a79dfa06f
chapman39 <chapman39@llnl.gov>
2026-02-05 10:56:30 -0800

    update tribol and mfem

--- scripts/spack/spack_repo/smith/packages/mfem/package.py
+++ scripts/spack/spack_repo/smith/packages/mfem/package.py
@@ -11,7 +11,7 @@ class Mfem(BuiltinMfem):
     # Note: Make sure this sha coincides with the git submodule
     # Note: We add a number to the end of the real version number to indicate that we have
     # moved forward past the release. Increment the last number when updating the commit sha.
-    version("4.9.0.3", commit="232853214d99a7ef7a8fb85bcf7a28ee92131115")
+    version("4.9.0.4", commit="8efb9483bc8353cfcda3cc2531427f048f606e01")
 
     variant('asan', default=False, description='Add Address Sanitizer flags')
 
