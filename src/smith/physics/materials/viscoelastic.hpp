57528772246f4d4cd7baa023c032c54f6ca688ba
Brandon Talamini <talamini1@llnl.gov>
2026-03-13 17:54:22 -0700

    Fix finite deformation stress pullback error and add WLF temperature-dependent shift

--- src/smith/physics/materials/viscoelastic.hpp
+++ src/smith/physics/materials/viscoelastic.hpp
@@ -23,45 +23,80 @@ struct Viscoelastic {
   template <typename T> using InternalState = tensor<T, dim*dim>;  ///< Fv
   template <typename T> using Tensor = tensor<T, dim, dim>;
 
+  /**
+   * Stress due to the equilibrium branch.
+   *
+   * The Hencky model is used here.
+   */
   template <typename T1, typename T2>
   SMITH_HOST_DEVICE auto equilibrium_stress(const Tensor<T1>& H, T2 theta) const
   {
-    auto CmI = H + transpose(H) + transpose(H)*H;
-    auto E = 0.5*logIp_symm(CmI);
-    auto Ee = E - alpha_inf*(theta - theta_r)*Identity<dim>();
-    auto M = 2.0*G_inf*dev(Ee) + K_inf*tr(Ee)*Identity<dim>();
+    // The spatial version of Hencky elasticity is used as it avoids the need
+    // to compute the rotation tensor.
+    auto BmI = H + transpose(H) + H*transpose(H);
+    auto E = 0.5*logIp_symm(BmI);
+    auto Em = E - alpha_inf*(theta - theta_sf)*Identity<dim>();
+    auto M = 2.0*G_inf*dev(Em) + K_inf*tr(Em)*Identity<dim>();
     // pull back to Piola stress
+    // Since the response is isotropic, the conjugate stress M coincides with
+    // the Kirchhoff stress.
     auto F = H + Identity<dim>();
     return M*inv(transpose(F));
   }
 
-  template <typename T1>
-  SMITH_HOST_DEVICE auto kinetic(T1 tau_bar) {
-    return tau_bar / eta_0;
+  /**
+   * WLF shift factor for time-temperature superposition 
+   */
+  template <typename T>
+  SMITH_HOST_DEVICE auto shift_factor(T theta) const {
+    auto dT = theta - theta_r;
+    using std::pow;
+    return pow(10.0, -C1*dT/(C2 + dT));
   }
 
+  /**
+   * Compute the current Piola stress and the new viscous deformation tensor
+   */
   template <typename T1, typename T2, typename T3>
   SMITH_HOST_DEVICE auto update(const InternalState<T1>& Q, double dt, const Tensor<T2>& du_dX, T3 theta) const
   {
     auto P_inf = equilibrium_stress(du_dX, theta);
 
-    // Consider 
+    // Compute the stress in the spring-dashpot branch.
+    // Possible generalizations:
+    // - add more branches, giving more distinct relaxation time scales
+    // - add spring-dashpot branch(es) for the volumetric response
+    // - make the viscosity-strain rate relationship nonlinear, such as a power law
+
+    // Reshape the flattened inelastic distortion back into a tensor
     auto Fv = make_tensor<dim, dim>([&Q](int i, int j) { return Q[dim*i + j]; });
     auto F = du_dX + Identity<dim>();
     auto Fe = F*inv(Fv);
     auto Ce = transpose(Fe)*Fe;
     auto Ee = 0.5*log_symm(Ce);
     auto devM = 2.0*G_0*dev(Ee);
-    auto M = devM;
+    auto M = devM; // + volumetric part, if a viscous volumetric response is added
     auto tau_bar = std::sqrt(0.5)*norm(devM);
+    // Guard against division by zero.
+    // Value of the denominator in the tau_bar = 0 case is unimportant,
+    // since nothing evolves in that case.
     auto denom = (tau_bar > 0)? (tau_bar) : (1.0 + tau_bar);
     auto N = 0.5*devM/denom;
     
-    auto dg = tau_bar/(eta_0/dt + G_0);
+    auto a = shift_factor(theta);
+    // Change in equivalent shear strain across dashpot.
+    // If the viscosity relation is made nonlinear, this explicit relation will
+    // be replaced with a nonlinear solve.
+    auto dg = tau_bar/(a*eta_0/dt + G_0);
+    // update elastic predictor stress
     M -= 2*G_0*dg*N;
-    auto P_0 = M*inv(transpose(F));
-
+    // update inelastic distortion tensor
     auto Fv_new = exp_symm(dg*N)*Fv;
+    // Change stress measure to Piola
+    Fe = F*inv(Fv_new);
+    auto P_0 = transpose(inv(Fv_new)*M*inv(Fe));
+
+    // Flatten the inelastic distortion tensor
     auto internal_state_new = make_tensor<dim*dim>(
       [&Fv_new](int ij) { 
         int i = ij / dim;
@@ -95,11 +130,15 @@ struct Viscoelastic {
   double K_inf;  ///< equiibrium bulk modulus
   double G_inf;  ///< equilibrium shear modulus
   double alpha_inf; ///< equilibrium thermal expansion coefficient
+  double theta_sf; /// < reference temperature for thermal expansion (that is, stress-free temperature)
 
   double G_0; ///< shear modulus in branch 0
   double eta_0; ///< viscosity in branch 0
 
-  double theta_r; /// < reference temperature for thermal expansion
+  double theta_r; ///< reference temperature for temperature-dependent behaviors
+  double C1; ///< first WLF factor (dimensionless)
+  double C2; ///< second SLF factor (temperature units)
+
   double rho_r; ///< density in the reference configuration
 };
 
