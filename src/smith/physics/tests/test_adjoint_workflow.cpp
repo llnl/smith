61fbd61c9390c0d6d67a6fc1743009dfef344bbb
EB Chin <chin23@llnl.gov>
2026-06-08 22:35:17 -0700

    update test to just static

--- src/smith/physics/tests/test_adjoint_workflow.cpp
+++ src/smith/physics/tests/test_adjoint_workflow.cpp
@@ -39,8 +39,12 @@ namespace {
 
 constexpr int p = 1;
 constexpr int dim = 2;
+constexpr int contact_dim = 3;
 
 using ParametricSolid = SolidMechanics<p, dim, Parameters<H1<1>, H1<1>>>;
+#ifdef SMITH_USE_TRIBOL
+using ContactSolid = SolidMechanicsContact<p, contact_dim>;
+#endif
 
 const std::string mesh_tag {"mesh"};
 const std::string physics_prefix {"solid"};
@@ -53,16 +57,26 @@ struct AdjointWorkflowResult {
   double shape_norm {};
 };
 
-constexpr int num_time_steps = 3;
+struct PhysicsCase {
+  std::string name;
+  std::shared_ptr<Mesh> mesh;
+  std::unique_ptr<BasePhysics> physics;
+  std::string adjoint_dual_name;
+  std::string adjoint_dual_domain_name;
+  int adjoint_dual_direction {};
+  std::string validation_dual_name;
+  bool check_parameter_sensitivities {};
+};
+
 constexpr double time_step = 1.0;
 
-std::shared_ptr<Mesh> createMesh()
+std::shared_ptr<Mesh> createSolidMesh()
 {
   const std::string filename = SMITH_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh";
   return std::make_shared<Mesh>(buildMeshFromFile(filename), mesh_tag, 1, 0);
 }
 
-std::unique_ptr<ParametricSolid> createSolidSolver(std::shared_ptr<Mesh> mesh)
+std::unique_ptr<BasePhysics> createSolidSolver(std::shared_ptr<Mesh> mesh)
 {
   auto nonlinear_options = NonlinearSolverOptions{.nonlin_solver = NonlinearSolver::Newton,
                                                   .relative_tol = 1.0e-15,
@@ -99,85 +113,17 @@ std::unique_ptr<ParametricSolid> createSolidSolver(std::shared_ptr<Mesh> mesh)
   return solid;
 }
 
-FiniteElementState createReactionDirection(const BasePhysics& solid_solver, std::shared_ptr<Mesh> mesh, int direction)
-{
-  const FiniteElementDual& reactions = solid_solver.dual("reactions");
-
-  FiniteElementState reaction_directions(reactions.space(), "reaction_directions");
-  reaction_directions = 0.0;
-
-  mfem::VectorFunctionCoefficient reaction_direction(dim, [direction](const mfem::Vector& /*x*/, mfem::Vector& value) {
-    value = 0.0;
-    value[direction] = 1.0;
-  });
-
-  reaction_directions.project(reaction_direction, mesh->domain("essential_boundary"));
-
-  return reaction_directions;
-}
-
-AdjointWorkflowResult runSolidSequence(ParametricSolid& solid, std::shared_ptr<Mesh> mesh)
-{
-  solid.resetStates();
-  for (int step = 0; step < num_time_steps; ++step) {
-    solid.advanceTimestep(time_step);
-    EXPECT_EQ(step + 1, solid.cycle());
-    EXPECT_GT(solid.state("displacement").Norml2(), 0.0);
-    EXPECT_GT(solid.dual("reactions").Norml2(), 0.0);
-  }
-
-  solid.resetAdjointStates();
-
-  const auto& displacement = solid.state("displacement");
-  const auto& reactions = solid.dual("reactions");
-
-  FiniteElementDual displacement_adjoint_load(displacement.space(), "displacement_adjoint_load");
-  displacement_adjoint_load = 1.0;
-
-  auto reaction_adjoint_load = createReactionDirection(solid, mesh, 1);
-
-  FiniteElementDual parameter0_sensitivity(solid.parameter(0).space(), "parameter0_sensitivity");
-  parameter0_sensitivity = 0.0;
-  FiniteElementDual parameter1_sensitivity(solid.parameter(1).space(), "parameter1_sensitivity");
-  parameter1_sensitivity = 0.0;
-  FiniteElementDual shape_sensitivity(solid.shapeDisplacement().space(), "shape_sensitivity");
-  shape_sensitivity = 0.0;
-
-  for (int step = num_time_steps; step > 0; --step) {
-    const auto& checkpointed_displacement = solid.loadCheckpointedState("displacement", solid.cycle());
-    const auto& checkpointed_reactions = solid.loadCheckpointedDual("reactions", solid.cycle());
-    EXPECT_GT(checkpointed_displacement.Norml2(), 0.0);
-    EXPECT_GT(checkpointed_reactions.Norml2(), 0.0);
-
-    solid.setAdjointLoad({{"displacement", displacement_adjoint_load}});
-    solid.setDualAdjointBcs({{"reactions", reaction_adjoint_load}});
-    solid.reverseAdjointTimestep();
-
-    parameter0_sensitivity += solid.computeTimestepSensitivity(0);
-    parameter1_sensitivity += solid.computeTimestepSensitivity(1);
-    shape_sensitivity += solid.computeTimestepShapeSensitivity();
-    EXPECT_EQ(step - 1, solid.cycle());
-  }
-
-  AdjointWorkflowResult result;
-  result.displacement_norm = displacement.Norml2();
-  result.reaction_norm = reactions.Norml2();
-  result.parameter0_norm = parameter0_sensitivity.Norml2();
-  result.parameter1_norm = parameter1_sensitivity.Norml2();
-  result.shape_norm = shape_sensitivity.Norml2();
-  return result;
-}
-
 #ifdef SMITH_USE_TRIBOL
-using ContactSolid = SolidMechanicsContact<p, 3>;
-
 std::shared_ptr<Mesh> createContactMesh()
 {
   const std::string filename = SMITH_REPO_DIR "/data/meshes/contact_two_blocks.g";
-  return std::make_shared<Mesh>(buildMeshFromFile(filename), "contact_mesh", 0, 0);
+  auto mesh = std::make_shared<Mesh>(buildMeshFromFile(filename), "contact_mesh", 0, 0);
+  mesh->addDomainOfBoundaryElements("two", by_attr<contact_dim>(2));
+  mesh->addDomainOfBoundaryElements("four", by_attr<contact_dim>(4));
+  return mesh;
 }
 
-std::unique_ptr<ContactSolid> createContactSolver(std::shared_ptr<Mesh> mesh)
+std::unique_ptr<BasePhysics> createContactSolver(std::shared_ptr<Mesh> mesh)
 {
   auto nonlinear_options = NonlinearSolverOptions{.nonlin_solver = NonlinearSolver::Newton,
                                                   .relative_tol = 1.0e-13,
@@ -190,17 +136,14 @@ std::unique_ptr<ContactSolid> createContactSolver(std::shared_ptr<Mesh> mesh)
 
   solid_mechanics::NeoHookean material{1.0, 10.0, 0.25};
   solid->setMaterial(material, mesh->entireBody());
-
-  mesh->addDomainOfBoundaryElements("support", by_attr<3>(2));
-  mesh->addDomainOfBoundaryElements("driven_surface", by_attr<3>(4));
-  solid->setFixedBCs(mesh->domain("support"));
+  solid->setFixedBCs(mesh->domain("two"));
   solid->setDisplacementBCs(
-      [](tensor<double, 3>, double) {
-        tensor<double, 3> value {};
-        value[1] = -0.1;
-        return value;
+      [](tensor<double, contact_dim> x, double) {
+        auto displacement = 0.0 * x;
+        displacement[1] = -0.1;
+        return displacement;
       },
-      mesh->domain("driven_surface"), Component::ALL);
+      mesh->domain("four"), Component::ALL);
 
   solid->addContactInteraction(0, {3}, {5},
                                ContactOptions{.method = ContactMethod::SingleMortar,
@@ -212,99 +155,133 @@ std::unique_ptr<ContactSolid> createContactSolver(std::shared_ptr<Mesh> mesh)
   solid->completeSetup();
   return solid;
 }
+#endif
 
-AdjointWorkflowResult runContactSequence(ContactSolid& solid, std::shared_ptr<Mesh> mesh)
+FiniteElementState createDualDirection(const BasePhysics& physics, std::shared_ptr<Mesh> mesh, const std::string& dual_name,
+                                       const std::string& domain_name, int direction)
 {
-  solid.resetStates();
-  for (int step = 0; step < num_time_steps; ++step) {
-    solid.advanceTimestep(time_step);
-    EXPECT_EQ(step + 1, solid.cycle());
-    EXPECT_GT(solid.state("displacement").Norml2(), 0.0);
-    EXPECT_GT(solid.dual("contact_force_0").Norml2(), 0.0);
-  }
+  const FiniteElementDual& dual = physics.dual(dual_name);
 
-  solid.resetAdjointStates();
+  FiniteElementState dual_direction(dual.space(), "dual_direction");
+  dual_direction = 0.0;
 
-  const auto& displacement = solid.state("displacement");
-  const auto& contact_force = solid.dual("contact_force_0");
+  const int vdim = dual.space().GetVDim();
+  mfem::VectorFunctionCoefficient direction_coefficient(vdim, [direction](const mfem::Vector& /*x*/, mfem::Vector& value) {
+    value = 0.0;
+    value[direction] = 1.0;
+  });
+
+  dual_direction.project(direction_coefficient, mesh->domain(domain_name));
+
+  return dual_direction;
+}
+
+AdjointWorkflowResult runStaticAdjointPass(BasePhysics& physics, const PhysicsCase& test_case)
+{
+  physics.resetAdjointStates();
+  EXPECT_EQ(1, physics.cycle());
+
+  const auto& displacement = physics.state("displacement");
+  const auto& validation_dual = physics.dual(test_case.validation_dual_name);
+  const auto checkpointed_displacement = physics.loadCheckpointedState("displacement", physics.cycle());
+  const auto checkpointed_validation_dual = physics.loadCheckpointedDual(test_case.validation_dual_name, physics.cycle());
+  EXPECT_GT(checkpointed_displacement.Norml2(), 0.0);
+  EXPECT_GT(checkpointed_validation_dual.Norml2(), 0.0);
 
   FiniteElementDual displacement_adjoint_load(displacement.space(), "displacement_adjoint_load");
   displacement_adjoint_load = 1.0;
 
-  FiniteElementState contact_force_adjoint_load(contact_force.space(), "contact_force_adjoint_load");
-  contact_force_adjoint_load = 0.0;
-  mfem::VectorFunctionCoefficient contact_force_direction(3, [](const mfem::Vector&, mfem::Vector& value) {
-    value = 0.0;
-    value[1] = 1.0;
-  });
-  contact_force_adjoint_load.project(contact_force_direction, mesh->domain("driven_surface"));
+  auto dual_adjoint_load = createDualDirection(physics, test_case.mesh, test_case.adjoint_dual_name,
+                                              test_case.adjoint_dual_domain_name, test_case.adjoint_dual_direction);
 
-  FiniteElementDual shape_sensitivity(solid.shapeDisplacement().space(), "shape_sensitivity");
-  shape_sensitivity = 0.0;
+  physics.setAdjointLoad({{"displacement", displacement_adjoint_load}});
+  physics.setDualAdjointBcs({{test_case.adjoint_dual_name, dual_adjoint_load}});
+  physics.reverseAdjointTimestep();
+  EXPECT_EQ(0, physics.cycle());
 
-  for (int step = num_time_steps; step > 0; --step) {
-    const auto& checkpointed_displacement = solid.loadCheckpointedState("displacement", solid.cycle());
-    EXPECT_GT(checkpointed_displacement.Norml2(), 0.0);
-
-    solid.setAdjointLoad({{"displacement", displacement_adjoint_load}});
-    solid.setDualAdjointBcs({{"contact_force_0", contact_force_adjoint_load}});
-    solid.reverseAdjointTimestep();
-
-    shape_sensitivity += static_cast<BasePhysics&>(solid).computeTimestepShapeSensitivity();
-    EXPECT_EQ(step - 1, solid.cycle());
-  }
+  const auto& shape_sensitivity = physics.computeTimestepShapeSensitivity();
 
   AdjointWorkflowResult result;
   result.displacement_norm = displacement.Norml2();
-  result.reaction_norm = contact_force.Norml2();
+  result.reaction_norm = validation_dual.Norml2();
+  if (physics.parameterNames().size() > 0) {
+    result.parameter0_norm = physics.computeTimestepSensitivity(0).Norml2();
+  }
+  if (physics.parameterNames().size() > 1) {
+    result.parameter1_norm = physics.computeTimestepSensitivity(1).Norml2();
+  }
   result.shape_norm = shape_sensitivity.Norml2();
   return result;
 }
+
+std::vector<PhysicsCase> createPhysicsCases()
+{
+  std::vector<PhysicsCase> cases;
+
+  auto solid_mesh = createSolidMesh();
+  cases.push_back(PhysicsCase{.name = "solid_mechanics",
+                              .mesh = solid_mesh,
+                              .physics = createSolidSolver(solid_mesh),
+                              .adjoint_dual_name = "reactions",
+                              .adjoint_dual_domain_name = "essential_boundary",
+                              .adjoint_dual_direction = 1,
+                              .validation_dual_name = "reactions",
+                              .check_parameter_sensitivities = true});
+
+#ifdef SMITH_USE_TRIBOL
+  auto contact_mesh = createContactMesh();
+  cases.push_back(PhysicsCase{.name = "solid_mechanics_contact",
+                              .mesh = contact_mesh,
+                              .physics = createContactSolver(contact_mesh),
+                              .adjoint_dual_name = "reactions",
+                              .adjoint_dual_domain_name = "two",
+                              .adjoint_dual_direction = 1,
+                              .validation_dual_name = "contact_force_0",
+                              .check_parameter_sensitivities = false});
 #endif
+
+  return cases;
+}
 
 }  // namespace
 
-TEST(AdjointWorkflow, QuasistaticSolidMechanics)
+TEST(AdjointWorkflow, QuasistaticStaticAdjointSolveCanRepeatForBasePhysicsTypes)
 {
   MPI_Barrier(MPI_COMM_WORLD);
   axom::sidre::DataStore datastore;
-  StateManager::initialize(datastore, "adjoint_workflow_solid");
+  StateManager::initialize(datastore, "adjoint_workflow_static_repeat");
 
-  auto mesh = createMesh();
-  auto solid = createSolidSolver(mesh);
+  for (auto& test_case : createPhysicsCases()) {
+    SCOPED_TRACE(test_case.name);
+    auto& physics = *test_case.physics;
 
-  const auto first = runSolidSequence(*solid, mesh);
-  const auto second = runSolidSequence(*solid, mesh);
+    physics.resetStates();
+    physics.advanceTimestep(time_step);
+    EXPECT_EQ(1, physics.cycle());
+    EXPECT_NEAR(time_step, physics.time(), 1.0e-14);
+    EXPECT_GT(physics.state("displacement").Norml2(), 0.0);
+    EXPECT_GT(physics.dual(test_case.validation_dual_name).Norml2(), 0.0);
 
-  EXPECT_NEAR(first.displacement_norm, second.displacement_norm, 1.0e-12);
-  EXPECT_NEAR(first.reaction_norm, second.reaction_norm, 1.0e-12);
-  EXPECT_NEAR(first.parameter0_norm, second.parameter0_norm, 1.0e-12);
-  EXPECT_NEAR(first.parameter1_norm, second.parameter1_norm, 1.0e-12);
-  EXPECT_NEAR(first.shape_norm, second.shape_norm, 1.0e-12);
-  EXPECT_GT(first.parameter0_norm + first.parameter1_norm, 0.0);
-  EXPECT_GT(first.shape_norm, 0.0);
+    const auto first = runStaticAdjointPass(physics, test_case);
+    const auto second = runStaticAdjointPass(physics, test_case);
+
+    EXPECT_NEAR(first.displacement_norm, second.displacement_norm, 1.0e-12);
+    EXPECT_NEAR(first.reaction_norm, second.reaction_norm, 1.0e-12);
+    EXPECT_NEAR(first.parameter0_norm, second.parameter0_norm, 1.0e-12);
+    EXPECT_NEAR(first.parameter1_norm, second.parameter1_norm, 1.0e-12);
+    EXPECT_NEAR(first.shape_norm, second.shape_norm, 1.0e-12);
+
+    EXPECT_TRUE(std::isfinite(first.displacement_norm));
+    EXPECT_TRUE(std::isfinite(first.reaction_norm));
+    EXPECT_TRUE(std::isfinite(first.parameter0_norm));
+    EXPECT_TRUE(std::isfinite(first.parameter1_norm));
+    EXPECT_TRUE(std::isfinite(first.shape_norm));
+    if (test_case.check_parameter_sensitivities) {
+      EXPECT_GT(first.parameter0_norm + first.parameter1_norm, 0.0);
+    }
+    EXPECT_GT(first.shape_norm, 0.0);
+  }
 }
-
-#ifdef SMITH_USE_TRIBOL
-TEST(AdjointWorkflow, ContactSolidMechanics)
-{
-  MPI_Barrier(MPI_COMM_WORLD);
-  axom::sidre::DataStore datastore;
-  StateManager::initialize(datastore, "adjoint_workflow_contact");
-
-  auto mesh = createContactMesh();
-  auto solid = createContactSolver(mesh);
-
-  const auto first = runContactSequence(*solid, mesh);
-  const auto second = runContactSequence(*solid, mesh);
-
-  EXPECT_NEAR(first.displacement_norm, second.displacement_norm, 1.0e-12);
-  EXPECT_NEAR(first.reaction_norm, second.reaction_norm, 1.0e-12);
-  EXPECT_NEAR(first.shape_norm, second.shape_norm, 1.0e-12);
-  EXPECT_GT(first.reaction_norm, 0.0);
-  EXPECT_GT(first.shape_norm, 0.0);
-}
-#endif
 
 }  // namespace smith
 
