a943177c33730c6a447e909f372dd6a64c0a6107
thartland <tucker.hartland@gmail.com>
2026-06-11 16:50:31 -0700

    cleanup

--- examples/obstacle_design/obstacle_design_example.cpp
+++ examples/obstacle_design/obstacle_design_example.cpp
@@ -87,10 +87,7 @@ class SmithObstacleDesignProblem : public ObstacleDesignProblem {
   std::shared_ptr<mfem::HypreParMatrix> HuuE_;
   std::shared_ptr<mfem::HypreParMatrix> HuthE_;
   std::shared_ptr<mfem::HypreParMatrix> HththE_;
-  // ... objective and weak forms
-  // ... unique_ptrs to HypreParMatrix's in order
-  // ... that they manage memory + allow access to raw
-  // ... pointers to various evaluations
+
  public:
   SmithObstacleDesignProblem(ParamOptProblem* paramopt, std::vector<smith::FiniteElementState*> states,
                              std::shared_ptr<smith::Mesh> mesh, std::shared_ptr<ObjectiveT> design_objective,
@@ -117,6 +114,8 @@ int main(int argc, char* argv[])
   double ylength = 1.0;
   int nx = 10;
   int ny = 10;
+  int serial_refinement = 0;
+  int parallel_refinement = 0;
   int visualize = 0;
 
   // Solver options
@@ -131,6 +130,12 @@ int main(int argc, char* argv[])
   app.add_option("--ylength", ylength, "extent along y-axis")
       ->default_val("1.0")  // Matches value set above
       ->check(axom::CLI::PositiveNumber);
+  app.add_option("--serial-refinement", serial_refinement, "Serial refinement steps")
+      ->default_val("0")  // Matches value set above
+      ->check(axom::CLI::PositiveNumber);
+  app.add_option("--parallel-refinement", parallel_refinement, "Parallel refinement steps")
+      ->default_val("0")  // Matches value set above
+      ->check(axom::CLI::PositiveNumber);
   app.add_option("--visualize", visualize, "solution visualization")
       ->default_val("0")  // Matches value set above
       ->check(axom::CLI::Range(0, 1));
@@ -144,13 +149,16 @@ int main(int argc, char* argv[])
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
   axom::sidre::DataStore datastore;
-  smith::StateManager::initialize(datastore, "solid_dynamics");
+  const std::string simulation_tag = "obstacle_design";
+  const std::string mesh_tag = simulation_tag + "mesh";
+  smith::StateManager::initialize(datastore, simulation_tag + "_data");
 
   std::shared_ptr<smith::Mesh> mesh;
 
   bool generate_edges = false;
   mesh = std::make_shared<smith::Mesh>(
-      mfem::Mesh::MakeCartesian2D(nx, ny, element_shape, generate_edges, xlength, ylength), "this_mesh_name", 0, 0);
+      mfem::Mesh::MakeCartesian2D(nx, ny, element_shape, generate_edges, xlength, ylength), mesh_tag, serial_refinement,
+      parallel_refinement);
 
   /* TODO:
    * 1) utilize a weak form to define the objective of the "lower level" obstacle problem
@@ -243,142 +251,15 @@ int main(int argc, char* argv[])
   designoptimizer.RegularizePrimalHessian(1.e-10);
   designoptimizer.Mult(X0, Xf);
 
-  // auto conststates = getConstFieldPointers(states, params);
-  // auto objective_states = {conststates[0]};
-  // auto writer = createParaviewWriter(mesh->mfemParMesh(), objective_states, "obstacledesign");
+  auto conststates = getConstFieldPointers(states, params);
+  auto vis_states = {conststates[0]};
+  auto writer = createParaviewWriter(mesh->mfemParMesh(), vis_states, "obstacledesign");
 
-  // if (visualize)  {
-  //   mfem::Vector xf(Xf, 0, dimU);
-  //   states[0] = xf;
-  //   writer.write(0, 0.0, objective_states);
-  // }
-
-  // smith::FiniteElementState disp = smith::StateManager::newState(VectorSpace{}, "displacement", mesh->tag());
-  // smith::FiniteElementState velo = smith::StateManager::newState(VectorSpace{}, "velocity", mesh->tag());
-  // smith::FiniteElementState accel = smith::StateManager::newState(VectorSpace{}, "acceleration", mesh->tag());
-  // smith::FiniteElementState density = smith::StateManager::newState(DensitySpace{}, "density", mesh->tag());
-  // std::unique_ptr<smith::FiniteElementState> shape_disp =
-  //     std::make_unique<smith::FiniteElementState>(mesh->newShapeDisplacement());
-
-  // velo = 0.0;
-  // accel = 0.0;
-
-  // states = {disp, velo, accel};
-  // params = {density};
-
-  // std::string physics_name = "solid";
-
-  //// construct residual
-  // auto solid_mechanics_weak_form =
-  //     std::make_shared<SolidWeakFormT>(physics_name, mesh, states[FIELD::DISP].space(), getSpaces(params));
-
-  // SolidMaterial mat;
-  // mat.K = 1.0;
-  // mat.G = 0.5;
-  // solid_mechanics_weak_form->setMaterial(smith::DependsOn<0>{}, mesh->entireBodyName(), mat);
-
-  //// apply some traction boundary conditions
-  // std::string surface_name = "side";
-  // mesh->addDomainOfBoundaryElements(surface_name, smith::by_attr<dim>(1));
-  // solid_mechanics_weak_form->addBoundaryFlux(surface_name, [](auto /*x*/, auto n, auto /*t*/) { return 1.0 * n; });
-
-  // smith::tensor<double, dim> constant_force{};
-  // for (int i = 0; i < dim; i++) {
-  //   constant_force[i] = 1.e0;
-  // }
-
-  // solid_mechanics_weak_form->addBodyIntegral(mesh->entireBodyName(), [constant_force](double /* t */, auto x) {
-  //   return smith::tuple{constant_force, 0.0 * smith::get<smith::DERIVATIVE>(x)};
-  // });
-
-  //// construct constraints
-  // params[0] = 1.;
-
-  // using ObjectiveT =
-  //     smith::FunctionalObjective<dim, smith::Parameters<VectorSpace, DensitySpace>>;  // functional objective on
-  //                                                                                     // displacement/density
-
-  // double time = 0.0;
-  // double dt = 1.0;
-  // smith::TimeInfo time_info(time, dt, 0);
-  // auto all_states = getConstFieldPointers(states, params);
-  // auto objective_states = {all_states[FIELD::DISP], all_states[FIELD::DENSITY]};
-
-  // ObjectiveT::SpacesT param_space_ptrs{&all_states[FIELD::DISP]->space(), &all_states[FIELD::DENSITY]->space()};
-
-  // ObjectiveT mass_objective("mass constraining", mesh, param_space_ptrs);
-
-  // mass_objective.addBodyIntegral(smith::DependsOn<1>{}, mesh->entireBodyName(),
-  //                                [](double /*t*/, auto /*X*/, auto RHO) { return get<smith::VALUE>(RHO); });
-  // double mass = mass_objective.evaluate(time_info, shape_disp.get(), objective_states);
-
-  // smith::tensor<double, dim> initial_cg;  // center of gravity
-
-  // for (int i = 0; i < dim; ++i) {
-  //   auto cg_objective = std::make_shared<ObjectiveT>("translation " + std::to_string(i), mesh, param_space_ptrs);
-  //   cg_objective->addBodyIntegral(smith::DependsOn<0, 1>{}, mesh->entireBodyName(),
-  //                                 [i](double
-  //                                     /*time*/,
-  //                                     auto X, auto U, auto RHO) {
-  //                                   return (get<smith::VALUE>(X)[i] + get<smith::VALUE>(U)[i]) *
-  //                                   get<smith::VALUE>(RHO);
-  //                                 });
-  //   initial_cg[i] = cg_objective->evaluate(time_info, shape_disp.get(), objective_states) / mass;
-
-  //  constraints.push_back(cg_objective);
-  //}
-
-  // for (int i = 0; i < dim; ++i) {
-  //   auto center_rotation_objective =
-  //       std::make_shared<ObjectiveT>("rotation" + std::to_string(i), mesh, param_space_ptrs);
-  //   center_rotation_objective->addBodyIntegral(smith::DependsOn<0, 1>{}, mesh->entireBodyName(),
-  //                                              [i, initial_cg](double /*time*/, auto X, auto U, auto RHO) {
-  //                                                auto u = get<smith::VALUE>(U);
-  //                                                auto x = get<smith::VALUE>(X) + u;
-  //                                                auto dx = x - initial_cg;
-  //                                                auto x_cross_u = smith::cross(dx, u);
-  //                                                return x_cross_u[i] * get<smith::VALUE>(RHO);
-  //                                              });
-  //   constraints.push_back(center_rotation_objective);
-  // }
-
-  //// initialize displacement
-  // states[FIELD::DISP].setFromFieldFunction([](smith::tensor<double, dim> x) {
-  //   auto u = 0.0 * x;
-  //   return u;
-  // });
-
-  // auto writer = createParaviewWriter(mesh->mfemParMesh(), objective_states, "inertia_relief");
-  // if (visualize) {
-  //   writer.write(0, 0.0, objective_states);
-  // }
-  // auto non_const_states = getFieldPointers(states, params);
-  //// create an inertial relief problem
-  // InertialReliefProblem problem({non_const_states[FIELD::DISP], non_const_states[FIELD::DENSITY]}, non_const_states,
-  //                               mesh, solid_mechanics_weak_form, constraints);
-
-  //// optimization variables
-  // auto X0 = problem.GetOptimizationVariable();
-  // auto Xf = problem.GetOptimizationVariable();
-
-  //// define a homotopy solver for the inertia relief problem
-  // HomotopySolver solver(&problem);
-  //// set solver options
-  // solver.SetTol(nonlinear_absolute_tol);
-  // solver.SetMaxIter(nonlinear_max_iterations);
-  // solver.EnableRegularizedNewtonMode();
-  //// solve the inertia relief problem
-  // solver.SetPrintLevel(2);
-  // solver.Mult(X0, Xf);
-  //// extract displacement and Lagrange multipliers
-  // mfem::Vector displacement_sol = problem.GetDisplacement(Xf);
-  // mfem::Vector multiplier_sol = problem.GetLagrangeMultiplier(Xf);
-  // bool converged = solver.GetConverged();
-  // SLIC_ERROR_ROOT_IF(!converged, "Homotopy solver did not converge");
-  // double displacement_norm = mfem::GlobalLpNorm(2, displacement_sol.Norml2(), MPI_COMM_WORLD);
-  // double multiplier_norm = mfem::GlobalLpNorm(2, multiplier_sol.Norml2(), MPI_COMM_WORLD);
-  // SLIC_INFO_ROOT(std::format("||displacement|| = {}", displacement_norm));
-  // SLIC_INFO_ROOT(std::format("||multiplier|| = {}", multiplier_norm));
+  if (visualize) {
+    mfem::Vector uf(Xf, 0, dimU);
+    states[0] = uf;
+    writer.write(0, 0.0, vis_states);
+  }
 }
 
 double fRhs(const mfem::Vector& x)
