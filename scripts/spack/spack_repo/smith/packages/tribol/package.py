7eca4dae84a0e7fd3e4b641d25b616def1fb16b6
chapman39 <chapman39@llnl.gov>
2026-06-24 12:07:46 -0700

    update mfem tribol

--- scripts/spack/spack_repo/smith/packages/tribol/package.py
+++ scripts/spack/spack_repo/smith/packages/tribol/package.py
@@ -52,7 +52,7 @@ class Tribol(CachedCMakePackage, CudaPackage, ROCmPackage):
     # SMITH EDIT START
     # Note: We add a number to the end of the real version number to indicate that we have
     #  moved forward past the release. Increment the last number when updating the commit sha.
-    version("0.1.0.27", commit="7c24d3a02db3b2494a93b478be98f42a4ce3e6d7", submodules=True, preferred=True)
+    version("0.1.0.27", commit="58e3afb21cba69fb7f0f1a1d00ac1bb2d0c37559", submodules=True, preferred=True)
     # SMITH EDIT END
 
     # -----------------------------------------------------------------------
