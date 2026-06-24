fc956047d6a81e50c72490c29ba2f610673babeb
Alex Chapman <chapman39@llnl.gov>
2026-06-24 14:36:45 -0700

    update macos host config

--- host-configs/other/firion-macos_sonoma_aarch64-llvm@19.1.7.cmake
+++ host-configs/other/firion-macos_sonoma_aarch64-llvm@19.1.7.cmake
@@ -1,31 +1,31 @@
 #------------------------------------------------------------------------------
 # !!!! This is a generated file, edit at own risk !!!!
 #------------------------------------------------------------------------------
-# CMake executable path: /Applications/CMake.app/Contents/bin/cmake
+# CMake executable path: /opt/homebrew/bin/cmake
 #------------------------------------------------------------------------------
 
-set(CMAKE_PREFIX_PATH "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/caliper-2.12.1-hxxjlnwmha6vjgje6dea2wyvultgknh6;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/tribol-0.1.0.25-mzozpke6qx7fkat5hag6vq5cpkrdifns;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/adiak-0.4.1-xgzizqbcpumo4bmx2szwc7mi7w4ypnnz;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/axom-0.12.0.1-uzfd7ktc4j6sx3llbydbuo5fhgreat3e;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/conduit-0.9.5-lyssq2x3vphf2wzdwvoul6wb7crvzuy5;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/mfem-4.9.1.1-dbkkwaoyf4ybyam3cd26fc4nnhov2bti;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/raja-2025.09.0-zae76hywf774ofbrf6yuyzxxvqoqc7fl;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/umpire-2025.09.0-mv5jc4yowf4zfaze5g5ogrm2ywb6tslp;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/enzyme-0.0.249-psdkrh5mv6ccbgbzycwufqqo5oghh7bq;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/netcdf-c-4.7.4-fqsqv3tc7k4r4ujujrilvd3uyozrvwjz;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/slepc-3.21.2-yrlmh4z2ujudsckpps4bx2lzqsssd67q;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/sundials-7.5.0-uja4njsnnjxyx4w3x3lrsu5rnml7hhil;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/superlu-dist-8.1.2-lbjgxyvy5uu2sfbypx4svwufu5zukhll;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/hdf5-1.8.23-nak3dcmiy77567pkpcbcyw4xtzfdqzv6;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/arpack-ng-3.9.1-bznno6nlmosoue55qsmnve5bcu7sowtp;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/petsc-3.21.6-zdx6pw27gzh4tyoj3d23mkzigvtnbchm;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/hypre-2.33.0-clac2m2by6ipk2sqqmlafivopa2zhfaq;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/strumpack-8.0.0-q2iwdbvjaet6xflipf6pwxsg5zwkdefo;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/netlib-scalapack-2.2.2-hh7hjukcttu3vdad3kk3zqyusmra7fbv;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/parmetis-4.0.3-enrikgqr6rdqjkoue6ffs2hrvb323ymf;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/camp-2025.09.2-kcgtvejxjqejkeoqf7rvh3cfnvy4cgfj;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/none-none/compiler-wrapper-1.0-2jjj3oxuce4es264dmeeysmyyyak5ltr;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/none-none/gcc-runtime-15.1.0-jcn6zqcysbvswoot2j4bqvzmbkfftszz;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/metis-5.1.0-pveb3dmykql3darojls3yijhhg775qkh;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/fmt-11.0.2-upaideq5wujnkjl45eailranvxhfxxxe;/Users/chapman39/dev/smith/venv;/Applications/CMake.app/Contents;/opt/homebrew;/opt/homebrew;/opt/homebrew/opt/gcc@15;/opt/homebrew/opt/llvm@19;/opt/homebrew;/opt/homebrew;/Users/chapman39/dev/smith/venv;/opt/homebrew/opt/lapack;/opt/homebrew/opt/zlib" CACHE STRING "")
+set(CMAKE_PREFIX_PATH "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/caliper-2.14.0-6ywotfdhx4i5vznc6wfo5dyyl6gpcqhl;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/tribol-0.1.0.27-hnbofa7s5zbid7pjeec64k2nyslca5sy;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/adiak-0.4.1-lhlihwzmtrnyv2zhug3naei5h7wlf554;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/axom-0.14.0.1-zg6fjig5po6ikq32rl2omlkbdixpewgj;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/conduit-0.9.6-abugd2zqqzjgo3a5ydybp5mhpeisl73f;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/mfem-4.9.0.3-ri6l2vnizmta6em3npzi42ere4u2tn4f;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/raja-2025.12.2-sb3sqzaleafwtoormkfpp5oc4v226hwy;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/umpire-2025.12.0-sv6ocajbtca52edfpdirocdyy4qxo3aw;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/enzyme-0.0.266-snthealhn2pwubfxevig34e5mftvmyv6;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/netcdf-c-4.10.0-gvbtppfikvfsliq7ocnm6ccsmukibmxw;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/slepc-3.21.2-adryxpnhuroarpedyfy4lhczfqtovntn;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/sundials-7.7.0-z3tdoma4nbwyffsm2gb627wsf5kn7ln2;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/superlu-dist-8.1.2-qbde37cpgcxbdsdoc3zdg42sf7dllmfh;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/hdf5-1.8.23-cmauqn3guzvrbbh37rn2hu5gmknebemo;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/arpack-ng-3.9.1-2ozoz7bwwd3hmq5fbndqykwfo7awzeh3;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/petsc-3.21.6-fka5dfwa4wnvr3dg46ciqi574ytf3u5c;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/hypre-2.33.0-fa5b4yg47xgcvyrrxvesboo6bnssg6il;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/strumpack-8.0.0-wg6njnuwxeiajpq6kqh6om74ycu5rav6;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/netlib-scalapack-2.2.3-tib62wqwzondnsjcsuzbbkl7bbsui6y7;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/parmetis-4.0.3-jfj5s7bjfvz4q74oiwlin2jz2ugmo3rn;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/camp-2025.12.0-5ryjss5hfzqsfyzj7jw54ec7fsln5zmu;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/none-none/compiler-wrapper-1.1.0-jti6zxsq6fb27aex57hfcgypopiojtpf;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/none-none/gcc-runtime-15.1.0-eiuw7zfyofxsdwh2zrqdhbwni7em5i6t;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/metis-5.1.0-qpeksn7dlgbwvfcsmik3qmhuy4vu7khf;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/fmt-11.0.2-5slcwwu22edkdx26f3yvy2g5ckz25q6o;/Users/chapman39/dev/smith/venv;/opt/homebrew;/opt/homebrew;/opt/homebrew;/opt/homebrew/opt/gcc@15;/opt/homebrew/opt/llvm@19;/opt/homebrew;/opt/homebrew;/opt/homebrew;/Users/chapman39/dev/smith/venv;/opt/homebrew/opt/zlib" CACHE STRING "")
 
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH "ON" CACHE STRING "")
 
-set(CMAKE_BUILD_RPATH "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/smith-develop-os7zbpsxrx4vtsrz5dcxx6q54nwwae7m/lib;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/smith-develop-os7zbpsxrx4vtsrz5dcxx6q54nwwae7m/lib64;/opt/homebrew/lib/gcc/15;" CACHE STRING "")
+set(CMAKE_BUILD_RPATH "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/smith-develop-43kq3jzbh6p3h7rew6y6qmacuizr7bzb/lib;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/smith-develop-43kq3jzbh6p3h7rew6y6qmacuizr7bzb/lib64;/opt/homebrew/lib/gcc/15;/opt/homebrew/Cellar/gcc/15.2.0_1/lib/gcc/current/gcc/aarch64-apple-darwin25/15;/opt/homebrew/Cellar/gcc/15.2.0_1/lib/gcc/current" CACHE STRING "")
 
-set(CMAKE_INSTALL_RPATH "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/smith-develop-os7zbpsxrx4vtsrz5dcxx6q54nwwae7m/lib;/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7/smith-develop-os7zbpsxrx4vtsrz5dcxx6q54nwwae7m/lib64;/opt/homebrew/lib/gcc/15;" CACHE STRING "")
+set(CMAKE_INSTALL_RPATH "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/smith-develop-43kq3jzbh6p3h7rew6y6qmacuizr7bzb/lib;/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7/smith-develop-43kq3jzbh6p3h7rew6y6qmacuizr7bzb/lib64;/opt/homebrew/lib/gcc/15;/opt/homebrew/Cellar/gcc/15.2.0_1/lib/gcc/current/gcc/aarch64-apple-darwin25/15;/opt/homebrew/Cellar/gcc/15.2.0_1/lib/gcc/current" CACHE STRING "")
 
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
 
 #------------------------------------------------------------------------------
 # Compilers
 #------------------------------------------------------------------------------
-# Compiler Spec: llvm@19.1.7/qt6jorlj5wfo7jv5cnu45gzgyj7vi753
+# Compiler Spec: llvm@19.1.7/rfo2ddjr7p35zmjg6cak7bax3joc3auy
 #------------------------------------------------------------------------------
 if(DEFINED ENV{SPACK_CC})
 
-  set(CMAKE_C_COMPILER "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/none-none/compiler-wrapper-1.0-2jjj3oxuce4es264dmeeysmyyyak5ltr/libexec/spack/clang/clang" CACHE PATH "")
+  set(CMAKE_C_COMPILER "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/none-none/compiler-wrapper-1.1.0-jti6zxsq6fb27aex57hfcgypopiojtpf/libexec/spack/clang/clang" CACHE PATH "")
 
-  set(CMAKE_CXX_COMPILER "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/none-none/compiler-wrapper-1.0-2jjj3oxuce4es264dmeeysmyyyak5ltr/libexec/spack/clang/clang++" CACHE PATH "")
+  set(CMAKE_CXX_COMPILER "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/none-none/compiler-wrapper-1.1.0-jti6zxsq6fb27aex57hfcgypopiojtpf/libexec/spack/clang/clang++" CACHE PATH "")
 
-  set(CMAKE_Fortran_COMPILER "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/none-none/compiler-wrapper-1.0-2jjj3oxuce4es264dmeeysmyyyak5ltr/libexec/spack/gcc/gfortran" CACHE PATH "")
+  set(CMAKE_Fortran_COMPILER "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/none-none/compiler-wrapper-1.1.0-jti6zxsq6fb27aex57hfcgypopiojtpf/libexec/spack/gcc/gfortran" CACHE PATH "")
 
 else()
 
@@ -65,53 +65,53 @@ set(ENABLE_OPENMP OFF CACHE BOOL "")
 # TPLs
 #------------------------------------------------------------------------------
 
-set(TPL_ROOT "/Users/chapman39/dev/smith/smith_tpls/2026-04-02/llvm-19.1.7" CACHE PATH "")
+set(TPL_ROOT "/Users/chapman39/dev/smith/smith_tpls/macos_sonoma_aarch64/2026-06-24/llvm-19.1.7" CACHE PATH "")
 
-set(AXOM_DIR "${TPL_ROOT}/axom-0.12.0.1-uzfd7ktc4j6sx3llbydbuo5fhgreat3e" CACHE PATH "")
+set(AXOM_DIR "${TPL_ROOT}/axom-0.14.0.1-zg6fjig5po6ikq32rl2omlkbdixpewgj" CACHE PATH "")
 
-set(CAMP_DIR "${TPL_ROOT}/camp-2025.09.2-kcgtvejxjqejkeoqf7rvh3cfnvy4cgfj" CACHE PATH "")
+set(CAMP_DIR "${TPL_ROOT}/camp-2025.12.0-5ryjss5hfzqsfyzj7jw54ec7fsln5zmu" CACHE PATH "")
 
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.9.5-lyssq2x3vphf2wzdwvoul6wb7crvzuy5" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.9.6-abugd2zqqzjgo3a5ydybp5mhpeisl73f" CACHE PATH "")
 
 set(LUA_DIR "/opt/homebrew" CACHE PATH "")
 
-set(MFEM_DIR "${TPL_ROOT}/mfem-4.9.1.1-dbkkwaoyf4ybyam3cd26fc4nnhov2bti" CACHE PATH "")
+set(MFEM_DIR "${TPL_ROOT}/mfem-4.9.0.3-ri6l2vnizmta6em3npzi42ere4u2tn4f" CACHE PATH "")
 
-set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.23-nak3dcmiy77567pkpcbcyw4xtzfdqzv6" CACHE PATH "")
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.23-cmauqn3guzvrbbh37rn2hu5gmknebemo" CACHE PATH "")
 
-set(HYPRE_DIR "${TPL_ROOT}/hypre-2.33.0-clac2m2by6ipk2sqqmlafivopa2zhfaq" CACHE PATH "")
+set(HYPRE_DIR "${TPL_ROOT}/hypre-2.33.0-fa5b4yg47xgcvyrrxvesboo6bnssg6il" CACHE PATH "")
 
-set(METIS_DIR "${TPL_ROOT}/metis-5.1.0-pveb3dmykql3darojls3yijhhg775qkh" CACHE PATH "")
+set(METIS_DIR "${TPL_ROOT}/metis-5.1.0-qpeksn7dlgbwvfcsmik3qmhuy4vu7khf" CACHE PATH "")
 
-set(PARMETIS_DIR "${TPL_ROOT}/parmetis-4.0.3-enrikgqr6rdqjkoue6ffs2hrvb323ymf" CACHE PATH "")
+set(PARMETIS_DIR "${TPL_ROOT}/parmetis-4.0.3-jfj5s7bjfvz4q74oiwlin2jz2ugmo3rn" CACHE PATH "")
 
-set(NETCDF_DIR "${TPL_ROOT}/netcdf-c-4.7.4-fqsqv3tc7k4r4ujujrilvd3uyozrvwjz" CACHE PATH "")
+set(NETCDF_DIR "${TPL_ROOT}/netcdf-c-4.10.0-gvbtppfikvfsliq7ocnm6ccsmukibmxw" CACHE PATH "")
 
-set(SUPERLUDIST_DIR "${TPL_ROOT}/superlu-dist-8.1.2-lbjgxyvy5uu2sfbypx4svwufu5zukhll" CACHE PATH "")
+set(SUPERLUDIST_DIR "${TPL_ROOT}/superlu-dist-8.1.2-qbde37cpgcxbdsdoc3zdg42sf7dllmfh" CACHE PATH "")
 
-set(ARPACK_DIR "${TPL_ROOT}/arpack-ng-3.9.1-bznno6nlmosoue55qsmnve5bcu7sowtp" CACHE PATH "")
+set(ARPACK_DIR "${TPL_ROOT}/arpack-ng-3.9.1-2ozoz7bwwd3hmq5fbndqykwfo7awzeh3" CACHE PATH "")
 
-set(ADIAK_DIR "${TPL_ROOT}/adiak-0.4.1-xgzizqbcpumo4bmx2szwc7mi7w4ypnnz" CACHE PATH "")
+set(ADIAK_DIR "${TPL_ROOT}/adiak-0.4.1-lhlihwzmtrnyv2zhug3naei5h7wlf554" CACHE PATH "")
 
 # AMGX not built
 
-set(CALIPER_DIR "${TPL_ROOT}/caliper-2.12.1-hxxjlnwmha6vjgje6dea2wyvultgknh6" CACHE PATH "")
+set(CALIPER_DIR "${TPL_ROOT}/caliper-2.14.0-6ywotfdhx4i5vznc6wfo5dyyl6gpcqhl" CACHE PATH "")
 
-set(ENZYME_DIR "${TPL_ROOT}/enzyme-0.0.249-psdkrh5mv6ccbgbzycwufqqo5oghh7bq" CACHE PATH "")
+set(ENZYME_DIR "${TPL_ROOT}/enzyme-0.0.266-snthealhn2pwubfxevig34e5mftvmyv6" CACHE PATH "")
 
-set(PETSC_DIR "${TPL_ROOT}/petsc-3.21.6-zdx6pw27gzh4tyoj3d23mkzigvtnbchm" CACHE PATH "")
+set(PETSC_DIR "${TPL_ROOT}/petsc-3.21.6-fka5dfwa4wnvr3dg46ciqi574ytf3u5c" CACHE PATH "")
 
-set(RAJA_DIR "${TPL_ROOT}/raja-2025.09.0-zae76hywf774ofbrf6yuyzxxvqoqc7fl" CACHE PATH "")
+set(RAJA_DIR "${TPL_ROOT}/raja-2025.12.2-sb3sqzaleafwtoormkfpp5oc4v226hwy" CACHE PATH "")
 
-set(SLEPC_DIR "${TPL_ROOT}/slepc-3.21.2-yrlmh4z2ujudsckpps4bx2lzqsssd67q" CACHE PATH "")
+set(SLEPC_DIR "${TPL_ROOT}/slepc-3.21.2-adryxpnhuroarpedyfy4lhczfqtovntn" CACHE PATH "")
 
-set(STRUMPACK_DIR "${TPL_ROOT}/strumpack-8.0.0-q2iwdbvjaet6xflipf6pwxsg5zwkdefo" CACHE PATH "")
+set(STRUMPACK_DIR "${TPL_ROOT}/strumpack-8.0.0-wg6njnuwxeiajpq6kqh6om74ycu5rav6" CACHE PATH "")
 
-set(SUNDIALS_DIR "${TPL_ROOT}/sundials-7.5.0-uja4njsnnjxyx4w3x3lrsu5rnml7hhil" CACHE PATH "")
+set(SUNDIALS_DIR "${TPL_ROOT}/sundials-7.7.0-z3tdoma4nbwyffsm2gb627wsf5kn7ln2" CACHE PATH "")
 
-set(UMPIRE_DIR "${TPL_ROOT}/umpire-2025.09.0-mv5jc4yowf4zfaze5g5ogrm2ywb6tslp" CACHE PATH "")
+set(UMPIRE_DIR "${TPL_ROOT}/umpire-2025.12.0-sv6ocajbtca52edfpdirocdyy4qxo3aw" CACHE PATH "")
 
-set(TRIBOL_DIR "${TPL_ROOT}/tribol-0.1.0.25-mzozpke6qx7fkat5hag6vq5cpkrdifns" CACHE PATH "")
+set(TRIBOL_DIR "${TPL_ROOT}/tribol-0.1.0.27-hnbofa7s5zbid7pjeec64k2nyslca5sy" CACHE PATH "")
 
 #------------------------------------------------------------------------------
 # Devtools
