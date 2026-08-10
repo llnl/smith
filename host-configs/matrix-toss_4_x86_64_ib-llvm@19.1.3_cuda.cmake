d9f3969193c02b06f47fec3c524b71f15e8ef2f3
Alex Tyler Chapman <chapman39@llnl.gov>
2026-08-10 10:47:19 -0700

    dane and matrix hc

---
+++ host-configs/matrix-toss_4_x86_64_ib-llvm@19.1.3_cuda.cmake
@@ -0,0 +1,164 @@
+#------------------------------------------------------------------------------
+# !!!! This is a generated file, edit at own risk !!!!
+#------------------------------------------------------------------------------
+# CMake executable path: /usr/tce/packages/cmake/cmake-3.30.5/bin/cmake
+#------------------------------------------------------------------------------
+
+set(CMAKE_PREFIX_PATH "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/caliper-2.15.0-syeljml54p3dyruzdlfs76xsxapapspf;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/tribol-0.1.0.28-k243v736udjnmdvyntqdkg5xrgvddh3o;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/adiak-0.4.1-mugoos2ngweef2sfv3h3hzamdobiidy7;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/axom-0.14.0.2-3cn5k5vfpcid76yencb5muoig5n4g7v3;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/conduit-0.9.6-4uztni6nznttkafhj3o35flpyi5dlbhi;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/mfem-4.9.1.4-r422pyhd3qt2v3pxvi2w4i7byeyi3hg6;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/raja-2025.12.2-s7gojqahzkarvf57sgue2z3kpkkyfens;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/umpire-2025.12.0-apmxcw5o23w6ck7qjqyuzays6bhwvaoq;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/amgx-2.5.0-7lez4ybdtmukeoud2u7zynqhwzysehgb;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/enzyme-0.0.266-l4d4gzpj5632pxsmjr4y7anmrk533i22;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/netcdf-c-4.10.0-nzlc3jnrrjpvuqex3whkprrubrt6njwg;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/slepc-3.25.1-awxy2zmqsxjfw6qjbhrdhc6avn35cok6;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/sundials-7.7.0-wox33sb33qpiz3vc4i5i74pah3ajnwm7;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/superlu-dist-8.1.2-fhwk2cxg4igtw2ezxf2px4ekt6zbxhwu;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/camp-2025.12.0-nfoxyucf44vyfs55oxhdsp4xcfmb4qhf;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/fmt-12.1.0-h42a64qzipimzjuc2dgzluebci3kxwch;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/hdf5-1.8.23-a4dez67ycuigqcbx4ula6cg6eppo4xvn;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/arpack-ng-3.9.1-5wyotkff3f6fw6qunmyw7d7any5yuxwf;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/petsc-3.25.4-qwmg5ncfipy3bnmutv4dkytyp7kedhia;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/hypre-2.33.0-53vzw7trph4m2hox5iwsqtk4tdu74pse;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/strumpack-8.0.0-3y4hyccrxhj2btyozcu3f6xjafuvgllb;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/netlib-scalapack-2.2.3-ipqfdckkgzuoibwkb5kxcn6p2uwvm3f6;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/parmetis-4.0.3-k6uxxx6yk2hi33gdwg4kj7owrj4yoixr;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/none-none/gcc-runtime-13.3.1-yw47es4h2fpvnmevwt7twetblmdvv3ad;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/metis-5.1.0-75lz5tk3pqsoo65umayxosg5zi4ztgal;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/none-none/compiler-wrapper-1.1.0-pbd2ayub7z6lkuzpvgsnzfsycl3xbuy3;/usr/workspace/smithdev/devtools/toss_4_x86_64_ib/latest/python-3.11.7;/usr/tce/packages/cmake/cmake-3.30.5;/usr/workspace/smithdev/devtools/toss_4_x86_64_ib/latest/cppcheck-2.9;/usr/tce/packages/cuda/cuda-12.9.1;/usr/workspace/smithdev/devtools/toss_4_x86_64_ib/latest/doxygen-1.9.8;/usr/tce/packages/gcc/gcc-13.3.1;/usr/tce/packages/clang/clang-19.1.3;/usr/tce/packages/mvapich2/mvapich2-2.3.7-clang-19.1.3-magic;/usr/tce/packages/python/python-3.9.12" CACHE STRING "")
+
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH "ON" CACHE STRING "")
+
+set(CMAKE_BUILD_RPATH "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/smith-develop-lvgkq6wb3jhvyzpw65qpwp4hihl6gn56/lib;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/smith-develop-lvgkq6wb3jhvyzpw65qpwp4hihl6gn56/lib64;;/usr/tce/backend/installations/linux-rhel8-x86_64/gcc-13.3.1/llvm-19.1.3-gy2lu5xbi4csr2k47emlajzfs5mlsd4g/lib/x86_64-unknown-linux-gnu;/opt/rh/gcc-toolset-13/root/usr/lib/gcc/x86_64-redhat-linux/13;/usr/tce/packages/clang/clang-19.1.3/lib;/collab/usr/global/tools/tce4/packages/gcc/gcc-13.3.1/lib/gcc/x86_64-redhat-linux/13" CACHE STRING "")
+
+set(CMAKE_INSTALL_RPATH "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/smith-develop-lvgkq6wb3jhvyzpw65qpwp4hihl6gn56/lib;/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3/smith-develop-lvgkq6wb3jhvyzpw65qpwp4hihl6gn56/lib64;;/usr/tce/backend/installations/linux-rhel8-x86_64/gcc-13.3.1/llvm-19.1.3-gy2lu5xbi4csr2k47emlajzfs5mlsd4g/lib/x86_64-unknown-linux-gnu;/opt/rh/gcc-toolset-13/root/usr/lib/gcc/x86_64-redhat-linux/13;/usr/tce/packages/clang/clang-19.1.3/lib;/collab/usr/global/tools/tce4/packages/gcc/gcc-13.3.1/lib/gcc/x86_64-redhat-linux/13" CACHE STRING "")
+
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+#------------------------------------------------------------------------------
+# Compilers
+#------------------------------------------------------------------------------
+# Compiler Spec: llvm@19.1.3/pa7jv5ejrkrucsgwtv4kde2cazhq3v6i
+#------------------------------------------------------------------------------
+if(DEFINED ENV{SPACK_CC})
+
+  set(CMAKE_C_COMPILER "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/none-none/compiler-wrapper-1.1.0-pbd2ayub7z6lkuzpvgsnzfsycl3xbuy3/libexec/spack/clang/clang" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/none-none/compiler-wrapper-1.1.0-pbd2ayub7z6lkuzpvgsnzfsycl3xbuy3/libexec/spack/clang/clang++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/none-none/compiler-wrapper-1.1.0-pbd2ayub7z6lkuzpvgsnzfsycl3xbuy3/libexec/spack/gcc/gfortran" CACHE PATH "")
+
+else()
+
+  set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-19.1.3-magic/bin/clang" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-19.1.3-magic/bin/clang++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc/gcc-13.3.1/bin/gfortran" CACHE PATH "")
+
+endif()
+
+set(CMAKE_C_FLAGS "-fPIC" CACHE STRING "")
+
+set(CMAKE_CXX_FLAGS "-fPIC" CACHE STRING "")
+
+set(CMAKE_Fortran_FLAGS "-fPIC" CACHE STRING "")
+
+#------------------------------------------------------------------------------
+# MPI
+#------------------------------------------------------------------------------
+
+set(MPI_C_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.7-clang-19.1.3-magic/bin/mpicc" CACHE PATH "")
+
+set(MPI_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.7-clang-19.1.3-magic/bin/mpicxx" CACHE PATH "")
+
+set(MPI_Fortran_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.7-clang-19.1.3-magic/bin/mpif90" CACHE PATH "")
+
+set(MPIEXEC_EXECUTABLE "/usr/bin/srun" CACHE PATH "")
+
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE STRING "")
+
+set(ENABLE_MPI ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# Hardware
+#------------------------------------------------------------------------------
+
+#------------------------------------------------
+# Cuda
+#------------------------------------------------
+
+set(CUDAToolkit_ROOT "/usr/tce/packages/cuda/cuda-12.9.1" CACHE PATH "")
+
+set(CMAKE_CUDA_COMPILER "${CUDAToolkit_ROOT}/bin/nvcc" CACHE PATH "")
+
+set(CMAKE_CUDA_HOST_COMPILER "${CMAKE_CXX_COMPILER}" CACHE PATH "")
+
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-12.9.1" CACHE PATH "")
+
+set(CMAKE_CUDA_ARCHITECTURES "90" CACHE STRING "")
+
+set(CMAKE_CUDA_FLAGS "" CACHE STRING "")
+
+set(ENABLE_OPENMP ON CACHE BOOL "")
+
+set(ENABLE_CUDA ON CACHE BOOL "")
+
+set(CMAKE_CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "")
+
+set(CMAKE_CUDA_FLAGS "-fPIC ${CMAKE_CUDA_FLAGS} --expt-extended-lambda --expt-relaxed-constexpr " CACHE STRING "" FORCE)
+
+# nvcc does not like gtest's 'pthreads' flag
+
+set(gtest_disable_pthreads ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# TPLs
+#------------------------------------------------------------------------------
+
+set(TPL_ROOT "/usr/WS2/smithdev/libs/smith/toss_4_x86_64_ib/matrix/2026_08_06_14_52_37/llvm-19.1.3" CACHE PATH "")
+
+set(AXOM_DIR "${TPL_ROOT}/axom-0.14.0.2-3cn5k5vfpcid76yencb5muoig5n4g7v3" CACHE PATH "")
+
+set(CAMP_DIR "${TPL_ROOT}/camp-2025.12.0-nfoxyucf44vyfs55oxhdsp4xcfmb4qhf" CACHE PATH "")
+
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.9.6-4uztni6nznttkafhj3o35flpyi5dlbhi" CACHE PATH "")
+
+set(LUA_DIR "/usr" CACHE PATH "")
+
+set(MFEM_DIR "${TPL_ROOT}/mfem-4.9.1.4-r422pyhd3qt2v3pxvi2w4i7byeyi3hg6" CACHE PATH "")
+
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.23-a4dez67ycuigqcbx4ula6cg6eppo4xvn" CACHE PATH "")
+
+set(HYPRE_DIR "${TPL_ROOT}/hypre-2.33.0-53vzw7trph4m2hox5iwsqtk4tdu74pse" CACHE PATH "")
+
+set(METIS_DIR "${TPL_ROOT}/metis-5.1.0-75lz5tk3pqsoo65umayxosg5zi4ztgal" CACHE PATH "")
+
+set(PARMETIS_DIR "${TPL_ROOT}/parmetis-4.0.3-k6uxxx6yk2hi33gdwg4kj7owrj4yoixr" CACHE PATH "")
+
+set(NETCDF_DIR "${TPL_ROOT}/netcdf-c-4.10.0-nzlc3jnrrjpvuqex3whkprrubrt6njwg" CACHE PATH "")
+
+set(SUPERLUDIST_DIR "${TPL_ROOT}/superlu-dist-8.1.2-fhwk2cxg4igtw2ezxf2px4ekt6zbxhwu" CACHE PATH "")
+
+set(ARPACK_DIR "${TPL_ROOT}/arpack-ng-3.9.1-5wyotkff3f6fw6qunmyw7d7any5yuxwf" CACHE PATH "")
+
+set(ADIAK_DIR "${TPL_ROOT}/adiak-0.4.1-mugoos2ngweef2sfv3h3hzamdobiidy7" CACHE PATH "")
+
+set(AMGX_DIR "${TPL_ROOT}/amgx-2.5.0-7lez4ybdtmukeoud2u7zynqhwzysehgb" CACHE PATH "")
+
+set(CALIPER_DIR "${TPL_ROOT}/caliper-2.15.0-syeljml54p3dyruzdlfs76xsxapapspf" CACHE PATH "")
+
+set(ENZYME_DIR "${TPL_ROOT}/enzyme-0.0.266-l4d4gzpj5632pxsmjr4y7anmrk533i22" CACHE PATH "")
+
+set(PETSC_DIR "${TPL_ROOT}/petsc-3.25.4-qwmg5ncfipy3bnmutv4dkytyp7kedhia" CACHE PATH "")
+
+set(RAJA_DIR "${TPL_ROOT}/raja-2025.12.2-s7gojqahzkarvf57sgue2z3kpkkyfens" CACHE PATH "")
+
+set(SLEPC_DIR "${TPL_ROOT}/slepc-3.25.1-awxy2zmqsxjfw6qjbhrdhc6avn35cok6" CACHE PATH "")
+
+set(STRUMPACK_DIR "${TPL_ROOT}/strumpack-8.0.0-3y4hyccrxhj2btyozcu3f6xjafuvgllb" CACHE PATH "")
+
+set(SUNDIALS_DIR "${TPL_ROOT}/sundials-7.7.0-wox33sb33qpiz3vc4i5i74pah3ajnwm7" CACHE PATH "")
+
+set(UMPIRE_DIR "${TPL_ROOT}/umpire-2025.12.0-apmxcw5o23w6ck7qjqyuzays6bhwvaoq" CACHE PATH "")
+
+set(TRIBOL_DIR "${TPL_ROOT}/tribol-0.1.0.28-k243v736udjnmdvyntqdkg5xrgvddh3o" CACHE PATH "")
+
+#------------------------------------------------------------------------------
+# Devtools
+#------------------------------------------------------------------------------
+
+set(DEVTOOLS_ROOT "/usr/WS2/smithdev/devtools/toss_4_x86_64_ib/2024_05_30_15_02_20/._view/psk2dcrijss6s4i6qmxplzthrzm3y7nh" CACHE PATH "")
+
+set(CLANGFORMAT_EXECUTABLE "/usr/tce/packages/clang/clang-19.1.3/bin/clang-format" CACHE PATH "")
+
+set(CLANGTIDY_EXECUTABLE "/usr/tce/packages/clang/clang-19.1.3/bin/clang-tidy" CACHE PATH "")
+
+set(ENABLE_DOCS ON CACHE BOOL "")
+
+set(SPHINX_EXECUTABLE "${DEVTOOLS_ROOT}/python-3.11.7/bin/sphinx-build" CACHE PATH "")
+
+set(CPPCHECK_EXECUTABLE "${DEVTOOLS_ROOT}/cppcheck-2.9/bin/cppcheck" CACHE PATH "")
+
+set(DOXYGEN_EXECUTABLE "${DEVTOOLS_ROOT}/doxygen-1.9.8/bin/doxygen" CACHE PATH "")
+
+
